b73c735c50e10afb1c63aa19c3c1916e817bba74
Ezequiel <ezequielsennesantos@gmail.com>
2022-10-05 08:50:56 -0300

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import {Link} from 'next/link'
 export default function Home() {
   return (
     <>
-    
+    <h1>Ola</h1>
+    <hr/>
+    <Link href = "http://www.google.com.br">google</Link>
     </>
   )
 }
